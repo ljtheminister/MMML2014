d0b8e35638a68d7abe98a5e5a44670df24c6b7bf
Nicholas Marinakis <ngm2110@columbia.edu>
2014-03-21 16:04:18 -0400

    Final version

--- scripts/Models.py
+++ scripts/Models.py
@@ -6,6 +6,9 @@ import numpy as np
 from sklearn import linear_model, preprocessing, cross_validation, svm, ensemble
 from RPI_SOS import RankingsCalculator
 from itertools import combinations
+from random import shuffle
+from math import log
+
 
 class ClassificationModels(object):
 
@@ -21,10 +24,13 @@ class ClassificationModels(object):
         self.GBT = ensemble.GradientBoostingClassifier()
         self.team_features = []
 
-    def train(self):
-        X = self.build_matrix()
+    def train(self, exclude_seasons=[]):
+        X = self.build_matrix(exclude_seasons)
         scaled_X = preprocessing.scale(X[:, :-1])
         Y = X[:, -1]
+        imputer = preprocessing.Imputer(missing_values='NaN', strategy='mean', axis=0)
+        imputer.fit(scaled_X)
+        scaled_X = imputer.transform(scaled_X)
         self.Logit.fit(scaled_X, Y)
         self.SVM.fit(scaled_X, Y)
         self.GBT.fit(scaled_X, Y)
@@ -51,9 +57,9 @@ class ClassificationModels(object):
         matrix = np.array(matrix)
         imputer = preprocessing.Imputer(missing_values='NaN', strategy='mean', axis=0)
         imputer.fit(matrix)
-        logit = self.Logit.predict_proba(preprocessing.scale(matrix))
+        logit = self.Logit.predict_proba(preprocessing.scale(imputer.transform(matrix)))
         SVM = self.SVM.predict_proba(preprocessing.scale(imputer.transform(matrix)))
-        GBT = self.GBT.predict_proba(preprocessing.scale(matrix))
+        GBT = self.GBT.predict_proba(preprocessing.scale(imputer.transform(matrix)))
         pred = []
         for i in range(len(logit)):
             pred.append((a*logit[i][1] + b*SVM[i][1] + c*GBT[i][1])/(a+b+c))
@@ -96,9 +102,9 @@ class ClassificationModels(object):
         temp_list += self.Ranks.tourney_seeds[season][h_team]
         return temp_list
 
-    def build_matrix(self, exclude_season=None):
+    def build_matrix(self, exclude_seasons=[]):
         output_list = []
-        seasons = [x for x in self.Ranks.tourney if x != exclude_season]
+        seasons = [x for x in self.Ranks.tourney if x not in exclude_seasons]
         for season in seasons:
             for (l_team, h_team) in self.Ranks.tourney[season]:
                 temp_list = self.build_row(season, l_team, h_team)
@@ -106,15 +112,37 @@ class ClassificationModels(object):
                 output_list.append(temp_list)
         return np.array(output_list)
 
+    def cross_validate(self):
+        seasons = [chr(x) for x in range(65, 83)]
+        shuffle(seasons)
+        test = seasons[:3]
+        train = seasons[3:]
+        self.train(exclude_seasons=test)
+        for a in range(1,6):
+            for b in range(1,6):
+                for c in range(1,6):
+                    for season in test:
+                        df = self.predict(season, a=a, b=b, c=c)
+                        #results =
+
+
+
+    def eval_log_loss(self, pred_frame, results_frame):
+        run_sum = 0
+        for i in range(len(pred_frame[0])):
+            y = results_frame[0][i]
+            y_hat = pred_frame[0][i]
+            run_sum += y * log(y_hat) + (1 - y) * log(1-y_hat)
+        return (-1.0/len(pred_frame[0])) * run_sum
+
+
+
 
 if __name__ == '__main__':
     CM = ClassificationModels('../raw_data/regular_season_results.csv',
                               '../raw_data/tourney_results.csv',
                               '../raw_data/tourney_seeds.csv',
                               '../raw_data/sagp_weekly_ratings.csv')
-    #L.train()
-    #print L.predict('S')
-    svm, l, gbt = CM.score()
-    print svm
-    print l
-    print gbt
+    CM.train()
+    print CM.score()
+    CM.predict('S').to_csv('predictions_7_25_power.csv')
